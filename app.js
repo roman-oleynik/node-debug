bc53756f55acd4240bfbf9b08370c7a979c17837
roman-oleynik <raman.alejnik@gmail.com>
2021-05-22 21:02:55 +0300

    init: initialize the app and fix issues that prevented it to work

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 var user = require('./controllers/usercontroller');
-var game = require('./controllers/gamecontroller')
+var game = require('./controllers/gamecontroller');
 
+
+var PORT = process.env.PORT;
 
 db.sync();
-app.use(require('body-parser'));
+// app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
-})
+app.listen(PORT, () =>
+  console.log(`App is running on ${PORT}`)
+);
